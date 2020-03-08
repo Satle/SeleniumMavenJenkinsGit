69f95c5a53dd2f7dfa6177bb09f14cfa91a9ec38
Satle <abhilasha.satle@gmail.com>
2020-03-08 13:13:17 +0530

    jhk

---
+++ FirstMavenProject/src/test/java/samplePackage/FirstMavinClass.java
@@ -0,0 +1,25 @@
+package samplePackage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstMavinClass {
+
+	@Test
+	public void test1() {
+
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "E:\\Jars\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		/* ******* this wait will apply to all the element associated to this driver. ***** */
+		driver.get("http://sampleapp.tricentis.com/102/app.php");
+		driver.findElement(By.xpath("//div[@class='main-navigation']//a[@id='nav_automobile']")).click();
+		System.out.println("**********Testing first mavin class");
+	}
+}
---
+++ FirstMavenProject/src/test/java/samplePackage/ThirdMavinClass.java
@@ -0,0 +1,12 @@
+package samplePackage;
+
+import org.testng.annotations.Test;
+
+public class ThirdMavinClass {
+	
+	@Test
+	public void test1(){
+		System.out.println("*************Third mavin class");
+	}
+
+}
